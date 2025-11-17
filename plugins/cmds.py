f7dd4cc7ba269a88edf8be98b0b76d4ad61719de
the-piro <thezake0077@gmail.com>
2025-11-17 19:03:20 +0530

    Update cmds.py

--- plugins/cmds.py
+++ plugins/cmds.py
@@ -4,15 +4,11 @@ from xtra import check_cmd, LINK_REGEX
 
 WELCOME = "Hello! Send me any MEGA link and I will check it."
 
-@Client.on_message(filters.text | filters.caption)
+@Client.on_message((filters.text | filters.caption) & ~filters.command(["start"]))
 async def auto_check_mega(client, message):
-    if message.text and message.text.startswith("/start"):
-        return
-
     text = message.text or message.caption or ""
     if not re.search(LINK_REGEX, text):
         return
-
     await check_cmd(client, message)
 
 @Client.on_message(filters.command("start"))
